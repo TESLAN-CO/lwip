034e32e52487c965213b6dfaeb06be8dae188e51
John <40263018+johndejohn@users.noreply.github.com>
2019-04-01 23:57:57 +0300

    Update teslan.c

--- src/core/teslan.c
+++ src/core/teslan.c
@@ -22,7 +22,7 @@
  */
 
 /*
- * Copyright (c) 2017-2019 UL CORP LTD.
+ * Copyright (c) 2019 UL CORP LTD.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
